4fd8340aff4490376e7fd351ab71d942b2d06a29
CleberAugustz <cleber.augustz@hotmail.com>
2021-06-29 14:43:38 -0300

    fix: added cors access

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: process.env.REACT_APP_API,
+    withCredentials: false,
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+    },
 });
 
 export default api;
